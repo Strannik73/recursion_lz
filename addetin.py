3ef6ae6d213a95829e3e8354af726c1adc83d186
Strannik73 <Santon@tutamail.com>
2024-12-16 22:06:01 +0300

    Delete addetin.py

--- addetin.py
+++
@@ -1,9 +0,0 @@
-def gen_bin(M, prefix=''):   # M - количество разрядов, prefix - сюда наращиваются 
-    if M == 0:
-        print(prefix)
-        return
- 
-    gen_bin(M - 1, prefix + '0')
-    gen_bin(M - 1, prefix + '1')
- 
-gen_bin()
