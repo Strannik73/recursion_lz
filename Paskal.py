58be88c07d4bb7b5ddc319d18cceab4009165540
Strannik37 <Santon@tutamail.com>
2024-12-16 21:09:51 +0300

    Paskal.py

---
+++ Paskal.py
@@ -0,0 +1,13 @@
+inp = int(input())
+
+def pascal(n, lol=None):
+    if lol is None:
+        lol = [[1]]
+    if n == 1:
+        return lol
+    else:
+        prev_row = lol[-1]
+        new_row = [1] + [sum(i) for i in zip(prev_row, prev_row[1:])] + [1]
+        return pascal(n - 1, lol + [new_row])
+    
+print(pascal(inp))
