3bb58e87250a49387f07e99ee7da39a3d9b5f6c2
Strannik73 <Santon@tutamail.com>
2024-12-16 15:05:25 +0300

    notation.py

--- notation.py
+++ notation.py
@@ -2,8 +2,8 @@ def func(number, scale):
         if scale > number:
             return str(number)
         return func(number // scale, scale) + str(number % scale)
+
+scale = int(input('система счисления : '))
+number = int(input('число : '))
  
-scale = int(input())
-number = int(input())
- 
-print(number, "(10)=", func(number, scale), "(", scale, ")", sep="")
+print('перевод : ',number, "(10)=", func(number, scale), "(", scale, ")", sep="")
