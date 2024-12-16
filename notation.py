5c7e800fbf79da738cdbed54bd7d2f98f459f178
Strannik37 <Santon@tutamail.com>
2024-12-16 21:39:34 +0300

    notation.py

--- notation.py
+++ notation.py
@@ -1,9 +1,19 @@
-def func(number, scale):
-        if scale > number:
-            return str(number)
-        return func(number // scale, scale) + str(number % scale)
+chislo = int(input('Введите число: '))
+sistem = int(input('Введите систему счисления: '))
 
-scale = int(input('система счисления : '))
-number = int(input('число : '))
- 
-print('перевод : ',number, "(10)=", func(number, scale), "(", scale, ")", sep="")
+
+def ert(N: int, T: int) -> str:
+    if N == 0:
+        return '0'
+
+    number = ''
+    while N > 0:
+        N, r = divmod(N, T)
+        if r > 9:
+            r = chr(ord('A') + r - 10)
+        number = str(r) + number
+    return number
+
+
+
+print('конечное число: ',ert(chislo, sistem),)
